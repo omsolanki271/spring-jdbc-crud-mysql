0954d352c7d06043fa21442e5bdbf09168361765
omsolanki271 <omsolanki271@gmail.com>
2026-02-23 01:33:23 +0530

    take value by user for operation

--- src/main/java/com/spring/jdbc/App.java
+++ src/main/java/com/spring/jdbc/App.java
@@ -1,5 +1,7 @@
 package com.spring.jdbc;
 
+import java.util.Scanner;
+
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -7,55 +9,67 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import com.spring.jdbc.dao.StudentDao;
 import com.spring.jdbc.entites.Student;
 
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println("My program started .....");
-        
-        ApplicationContext context = new ClassPathXmlApplicationContext("com/spring/jdbc/config.xml");
+public class App {
+	public static void main(String[] args) {
+		System.out.println("My program started .....");
 
-        //First implement this logic 
-        //This is normal way to insert data 
-        
-//        JdbcTemplate template = context.getBean("jdbcTemplate",JdbcTemplate.class);
-//        String query = "insert into student(stid,stnm,city) values(?,?,?)";        
-//        int result = template.update(query,106,"raghunath","GMC");
-//        System.out.println("number of rows inserted...."+result);
-        
-        //Second step Insert data 
-        /*
-        StudentDao bean = context.getBean("studentDao",StudentDao.class);
-        Student student = new Student();
-        student.setStid(109);
-        student.setStnm("Kajus");
-        student.setCity("Porbandar");
-        
-        int result = bean.insert(student);
-        System.out.println("number of rows inserted... "+result);
-        System.out.println(student.getStid() +" | "+ student.getStnm()+" | "+student.getCity());
-        */
-        
-        //Third Step update data
-        /*
-        StudentDao studentDao = context.getBean("studentDao",StudentDao.class);
-        
-        Student student = new Student();
-        student.setStid(106);
-        student.setStnm("Ravi");
-        student.setCity("Gandhinagar");
-        
-        int result = studentDao.change(student);
-        System.out.println(result + "Row updated....");
-        */
-        
-        //Fourth Step delete data
-        
-       StudentDao dao =  context.getBean("studentDao",StudentDao.class);
-       
-       
-       int del = dao.delete(106);
-       System.out.println(del + " Rows deleted....");
-       
-    }
+		ApplicationContext context = new ClassPathXmlApplicationContext("com/spring/jdbc/config.xml");
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("1. Insert");
+		System.out.println("2. Update");
+		System.out.println("3. Delete");
+
+		int choice = sc.nextInt();
+
+		StudentDao dao = context.getBean("studentDao", StudentDao.class);
+
+		switch (choice) {
+
+		case 1:
+			System.out.println("Enter Id:");
+			int id1 = sc.nextInt();
+			sc.nextLine();
+
+			System.out.println("Enter Name:");
+			String name1 = sc.nextLine();
+
+			System.out.println("Enter City:");
+			String city1 = sc.nextLine();
+
+			Student student1 = new Student(id1, name1, city1);
+			int insertResult = dao.insert(student1);
+			System.out.println(insertResult + " Row inserted");
+			break;
+
+		case 2:
+			System.out.println("Enter Id to update:");
+			int id2 = sc.nextInt();
+			sc.nextLine();
+
+			System.out.println("Enter New Name:");
+			String name2 = sc.nextLine();
+
+			System.out.println("Enter New City:");
+			String city2 = sc.nextLine();
+
+			Student student2 = new Student(id2, name2, city2);
+			int updateResult = dao.change(student2);
+			System.out.println(updateResult + " Row updated");
+			break;
+
+		case 3:
+			System.out.println("Enter Id to delete:");
+			int deleteId = sc.nextInt();
+
+			int deleteResult = dao.delete(deleteId);
+			System.out.println(deleteResult + " Row deleted");
+			break;
+
+		default:
+			System.out.println("Invalid choice");
+		}
+
+	}
 }
